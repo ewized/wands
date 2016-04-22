2c8b2fbdf9fbfa8a4365cc46bcfd25bd8bf97f8f
Joshua Rodriguez <ewized@gmail.com>
2016-04-21 17:41:50 -0700

    Tig magix plus linear algbra

--- src/main/java/com/ewized/wands/types/elements/IceWand.java
+++ src/main/java/com/ewized/wands/types/elements/IceWand.java
@@ -23,12 +23,51 @@ package com.ewized.wands.types.elements;
 
 import com.ewized.wands.types.Wand;
 import com.ewized.wands.types.WandType;
+import com.flowpowered.math.vector.Vector3d;
+import org.spongepowered.api.effect.particle.ParticleEffect;
+import org.spongepowered.api.effect.particle.ParticleTypes;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.chat.ChatTypes;
+import org.spongepowered.api.text.format.TextColors;
 
 public class IceWand implements Wand {
     @Override
     public void onAction(Player player, WandType wand) {
-        player.sendMessage(Text.of("Ice Wand"));
+        player.sendMessage(ChatTypes.ACTION_BAR, Text.of(TextColors.WHITE, "Ice Wand"));
+        Vector3d vector = player.getLocation().getPosition().add(0, 1.65, 0);
+        Vector3d pos = player.getHeadRotation();//.normalize();
+        double t = pos.getY() * Math.PI/180;
+
+
+        double o = 0.1;
+        double r = 0.1;
+        double rr = 0.01;
+        for (int i = 0; i < 360 * 2; i++) {
+            double theta = i * Math.PI / 180;
+            double y = r * Math.cos(theta) * Math.cos(pos.getY());
+            double z = r * Math.sin(theta) * Math.cos(pos.getY());
+            double yy = rr * Math.cos(theta) * Math.sin(pos.getY());
+            double zz = rr * Math.sin(theta) * Math.sin(pos.getY());
+            rr += 0.00125;
+            r += 0.0654;
+
+            double xxx = z * Math.cos(t) - y * Math.sin(t);
+            double zzz = z * Math.sin(t) + y * Math.cos(t);
+
+
+            double xxxx = zz * Math.cos(t) - yy * Math.sin(t);
+            double zzzz = zz * Math.sin(t) + yy * Math.cos(t);
+            ParticleEffect effect = ParticleEffect.builder()
+                    .type(ParticleTypes.SNOW_SHOVEL)
+                    .build();
+            ParticleEffect effect2 = ParticleEffect.builder()
+                    .type(ParticleTypes.SMOKE_NORMAL)
+                    .build();
+
+
+            player.getLocation().getExtent().spawnParticles(effect, vector.clone().add(xxxx,o += 0.0125, zzzz));
+            player.getLocation().getExtent().spawnParticles(effect2, vector.clone().add(xxx, o += 0.0879, zzz));
+        }
     }
 }
--- src/main/java/com/ewized/wands/types/elements/WaterWand.java
+++ src/main/java/com/ewized/wands/types/elements/WaterWand.java
@@ -23,12 +23,40 @@ package com.ewized.wands.types.elements;
 
 import com.ewized.wands.types.Wand;
 import com.ewized.wands.types.WandType;
+import com.flowpowered.math.vector.Vector2d;
+import com.flowpowered.math.vector.Vector3d;
+import org.spongepowered.api.effect.particle.ParticleEffect;
+import org.spongepowered.api.effect.particle.ParticleTypes;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.chat.ChatTypes;
+import org.spongepowered.api.text.format.TextColors;
 
 public class WaterWand implements Wand {
     @Override
     public void onAction(Player player, WandType wand) {
-        player.sendMessage(Text.of("Water Wand"));
+        player.sendMessage(ChatTypes.ACTION_BAR, Text.of(TextColors.BLUE, "Water Wand"));
+        Vector3d vector = player.getLocation().getPosition().add(0, 1.65, 0);
+        Vector3d pos = player.getHeadRotation();//.normalize();
+        double t = pos.getY() * Math.PI/180;
+
+        double o = 0.1;
+        double r = 0.1;
+        for (int i = 0; i < 360 * 8; i++) {
+            double theta = i * Math.PI / 180;
+            double x = (o += 0.00125);
+            double y = r * Math.cos(theta);
+            double z = r * Math.sin(theta);
+            r += 0.00125;
+
+            double xx = z * Math.cos(t) - x * Math.sin(t);
+            double zz = z * Math.sin(t) + x * Math.cos(t);
+            ParticleEffect effect = ParticleEffect.builder()
+                .type(ParticleTypes.DRIP_WATER)
+                .build();
+
+
+            player.getLocation().getExtent().spawnParticles(effect, vector.clone().add(xx, y, zz));
+        }
     }
 }
